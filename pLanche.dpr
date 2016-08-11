0e56e5c47f1193948e4649b1f493748e98034c75
Eduardo Henrique de Freitas <eduardo.freitas@datacoper.com.br>
2016-08-11 20:12:55 -0300

    1
    Criação dos arquivos

---
+++ pLanche.dpr
@@ -0,0 +1,14 @@
+program pLanche;
+
+uses
+  Vcl.Forms,
+  lanche in 'lanche.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
